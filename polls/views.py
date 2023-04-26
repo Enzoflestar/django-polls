d88cd28e9483b073f0b59ebd4a45e7f666c5ee0b
Enzo Lestar <enzo.lestar@aluno.ifsp.edu.br>
2023-04-26 09:35:00 -0300

    Hello World

--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Olá Mundo! Esta é a página inicial do aplicativo.")
+    return HttpResponse("Olá Mundo! Esta é a página inicial do aplicativo.")   
