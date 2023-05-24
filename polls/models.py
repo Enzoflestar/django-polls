088f1aa9edcfb6eadb9a56e7489c99eaa911b8db
Enzo Lestar <enzo.lestar@aluno.ifsp.edu.br>
2023-05-24 08:29:02 -0300

    atualização models

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,17 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=300)
     pub_date = models.DateTimeField("date published")
 
-     def __str__(self):
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
         return self.question_text
 
 
@@ -14,5 +20,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=300)
     votes = models.IntegerField(default=0)
     
-     def __str__(self):
-        return self.choice_text
+    def __str__(self):
+         return self.question_text
